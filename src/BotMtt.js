e5262ce1de1969e6e7a8a41a2a809cc0bb4397eb
henrion julien <julien.henrion@iut-valence.fr>
2017-05-03 23:44:36 +0200

    -adding profile command -updating prefix command / to ::

--- src/BotMtt.js
+++ src/BotMtt.js
@@ -1,23 +1,53 @@
-var Discord = require("discord.js");
-var bot = new Discord.Client();
+/*
+ *
+ *            Mettaton Bot ヽ༼ ಠ益ಠ ༽ﾉ
+ *
+ *            v1.0
+ *
+ */
+
+
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const token = "MjQ1OTU2OTQ2NTk3NzczMzEz.C-ujZA.ihTIyoAKtlpM3_lleVbD6aB9zKk";
 
 /*bot.on('voiceStateUpdate', (oldMember, newMember) => {
-  newMember.client.sendMessage(244530486263676928, "zbleh");
-  console.log(newMember.client);
-  console.log('je change de channel');
+newMember.client.sendMessage(244530486263676928, "zbleh");
+console.log(newMember.client);
+console.log('je change de channel');
 });*/
 
 bot.on("message", msg => {
-  if (msg.content.startsWith("/cute")) {
-    msg.delete();
-    msg.channel.sendFile("https://cdn.discordapp.com/attachments/240561455819128832/241257595237564416/a78.gif");
+  if (msg.content.startsWith("::")) {
+    var command = msg.content.substring(2,msg.content.length);
+    switch(command)
+    {
+      case "cute":
+        msg.delete();
+        msg.channel.sendFile("../res/cute_undyne.gif");
+        break;
+      case "zelote":
+        msg.delete();
+        msg.channel.send("(╬ ಠ益ಠ)");
+        break;
+      case "profile":
+        msg.delete();
+        var profile = new Discord.RichEmbed();
+        profile.setTitle(msg.author.username+" Profile");
+        profile.setThumbnail(msg.author.avatarURL);
+        profile.addBlankField();
+        profile.addField("test","ヽ༼ ಠ益ಠ ༽ﾉ");
+        profile.setFooter(msg.author.tag);
+        msg.channel.sendEmbed(profile);
+        break;
+      case "edit_profile":
+        msg.delete();
+        break;
     }
-  if (msg.content.startsWith("/zelote")) {
-    msg.delete();
-    msg.channel.sendMessage("(╬ ಠ益ಠ)");
-    }
-});
-bot.on('ready', () => {
-  console.log('I am ready!');
-});
-bot.login("MjQ1OTU2OTQ2NTk3NzczMzEz.C-ujZA.ihTIyoAKtlpM3_lleVbD6aB9zKk");
+
+      }
+    });
+    bot.on('ready', () => {
+      console.log('I am ready!');
+    });
+    bot.login(token);
