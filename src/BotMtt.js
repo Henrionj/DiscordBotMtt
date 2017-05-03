b7827de529b246841818cd78a8b3260f6761ef97
henrion julien <julien.henrion@iut-valence.fr>
2017-05-03 18:41:17 +0200

    adding evenemential action on message enabled only for a cute undyne (｡◕‿◕｡)

--- src/BotMtt.js
+++ src/BotMtt.js
@@ -1,9 +1,19 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
+/*bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  newMember.client.sendMessage(244530486263676928, "zbleh");
+  console.log(newMember.client);
+  console.log('je change de channel');
+});*/
 
+bot.on("message", msg => {
+  if (msg.content.startsWith("/cute")) {
+    msg.delete();
+    msg.channel.sendFile("https://cdn.discordapp.com/attachments/240561455819128832/241257595237564416/a78.gif");
+    }
+});
 bot.on('ready', () => {
   console.log('I am ready!');
 });
-
-bot.login("TokenBot");
+bot.login("MjQ1OTU2OTQ2NTk3NzczMzEz.CwxBEg.y5sCzk9q1HAJHsTYzCYh204IYNE");
